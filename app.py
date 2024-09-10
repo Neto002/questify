1364c01c9b8e6c487ebacf284ee57e0b35d63e48
antonionetooo002 <antonionetooo002@gmail.com>
2024-09-10 23:40:29 +0000

    initial templates

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("test.html")
+    return render_template("tasks.html")
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app.run(port=int(os.environ.get('PORT', 5000)))
 
 if __name__ == "__main__":
     main()
