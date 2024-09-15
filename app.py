96caa3d7736342f69482bad334091dd8574d8300
Antonio Neto <antoniogfneto92@hotmail.com>
2024-09-15 18:05:10 -0300

    interface inicial

--- app.py
+++ app.py
@@ -6,7 +6,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("tasks.html")
+    return render_template("index.html")
+
+@app.route("/missions")
+def missions():
+    return render_template("missions.html")
+
+@app.route("/rewards")
+def rewards():
+    return render_template("rewards.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 def main():
     app.run(port=int(os.environ.get('PORT', 5000)))
