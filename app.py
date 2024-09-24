0789fed22c21543f93fa2dda3c6dbc45baa6b034
assassinscraftg89 <assassinscraftg89@gmail.com>
2024-09-24 22:49:18 +0000

    Conectando no banco mais correção no frontend

--- app.py
+++ app.py
@@ -6,12 +6,44 @@ app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
+class Usuarios(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+    xp = db.Column(db.Integer, nullable=False)
+    nivel = db.Column(db.Integer, nullable=False)
+    pontos = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Usuarios {self.nome}>'
+
 with app.app_context():
     db.create_all()
 
+def createUser(nome, email, senha):
+    usuario = Usuarios(nome=nome, email=email, senha=senha, xp=0, nivel=1, pontos=0)
+    db.session.add(usuario)
+    db.session.commit()
+
+def table_exists(table_name):
+    try:
+        db.metadata.reflect(bind=db.engine)
+        db.metadata.tables[table_name]
+        return True
+    except KeyError:
+        return False
+
 @app.route("/")
 def index():
-    return render_template("index.html", todo=[["Tarefa 1",50],["Tarefa 2",30]])
+    return render_template("index.html", todo=[[1,"Tarefa 1",50],[2,"Tarefa 2",30]])
+
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:2678314680.
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:1603898920.
+@app.route("/test")
+def test():
+
+    return f'{table_exists("usuarios")}'
 
 @app.route("/missions")
 def missions():
