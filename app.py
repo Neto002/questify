bff53f3dd2c70841cb72dba324f513aee107ad4b
assassinscraftg89 <assassinscraftg89@gmail.com>
2024-09-26 15:28:50 +0000

    Criação de projetos no banco e integração com o frontend

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,6 +18,15 @@ class Usuarios(db.Model):
     def __repr__(self):
         return f'<Usuarios {self.nome}>'
 
+class Projetos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    integrantes = db.Column(db.String(255))
+    tarefas = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'Projetos {self.nome}>'
+
 with app.app_context():
     db.create_all()
 
@@ -25,6 +34,15 @@ def createUser(nome, email, senha):
     usuario = Usuarios(nome=nome, email=email, senha=senha, xp=0, nivel=1, pontos=0)
     db.session.add(usuario)
     db.session.commit()
+
+def createProject(nome):
+    projeto = Projetos(nome=nome, integrantes='', tarefas='')
+    db.session.add(projeto)
+    db.session.commit()
+
+def allProjects():
+    projetos = Projetos.query.all()
+    return projetos
 
 def table_exists(table_name):
     try:
@@ -36,7 +54,19 @@ def table_exists(table_name):
 
 @app.route("/")
 def index():
-    return render_template("index.html", todo=[["Tarefa 1",50,"Questify Front"],["Tarefa 2",30,"Questify Front"],["Tarefa 3",50,"Questify Back"],["Tarefa 4",30,"Questify Back"]])
+    #return f'{Projects()[0].nome}
+    projetos = allProjects()
+    for i in projetos:
+        if i.tarefas == '':
+            i.tarefas = 0
+        else:
+            i.tarefas = i.tarefas.split(', ')
+            i.tarefas = len(i.tarefas)
+    return render_template(
+        "index.html",
+        todo=[["Tarefa 1",50,"Questify Front",1],["Tarefa 2",30,"Questify Front",2],["Tarefa 3",50,"Questify Back",3],["Tarefa 4",30,"Questify Back",4]],
+        projects=projetos
+        )
 
 @app.route("/missions")
 def missions():
@@ -57,6 +87,12 @@ def login():
 @app.route("/register")
 def signup():
     return render_template("signup.html")
+
+@app.route("/add_project", methods=["POST"])
+def add_project():
+    project_name = request.form.get("projectName")
+    createProject(project_name)
+    return redirect(url_for("index"))
 
 def main():
     app.run(port=int(os.environ.get('PORT', 5000)))
